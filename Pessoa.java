7eccbfe94f5fa7a237655de9683744796141600e
ayrtonsenna <marcosennafontanarii@gmail.com>
2017-03-25 17:10:39 -0300

    Criando classe pessoa

---
+++ Pessoa.java
@@ -0,0 +1,11 @@
+public class Pessoa(){
+	String nome;
+	int idade;
+	String time;
+
+public Pessoa(String nome, int idade, String time){
+	this.nome = nome;
+	this.idade = idade;
+	this.time;
+}
+}
